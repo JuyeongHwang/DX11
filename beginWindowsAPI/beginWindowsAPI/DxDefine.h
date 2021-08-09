fb8841a6272ebc7e0d930b68d1e54a05d56bd76e
황주영 <49023736+JuyeongHwang@users.noreply.github.com>
2021-08-09 17:24:44 +0900

    follow the tutorial
    https://copynull.tistory.com/238?category=649932

---
+++ beginWindowsAPI/beginWindowsAPI/DxDefine.h
@@ -0,0 +1,23 @@
+#pragma once
+
+/////////////
+// LINKING //
+/////////////
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
+
+
+//////////////
+// INCLUDES //
+//////////////
+#include <d3d11.h>
+#include <d3dcompiler.h>
+#include <directxmath.h>
+using namespace DirectX;
+
+
+///////////////////////////
+//  warning C4316 Ã³¸®¿ë  //
+///////////////////////////
+#include "AlignedAllocationPolicy.h"
